6481f4e07e957596fda01fdd8f3442f0463ade78
agusfernandez <agustina.fer91@gmail.com>
2021-11-07 23:25:25 -0300

    import createStores, add reducers in global index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+//conexion entre react y redux
+import {Provider} from 'react-redux';
 
+//creacion del store y cargar la extecion del redux
+const newstore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+)
+// le paso la etoqueta provider para que envuelva a toda la app y le paso la propiedad store con newstore
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={newstore}>
+       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
